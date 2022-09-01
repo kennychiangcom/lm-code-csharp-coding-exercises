fe5f6d747a3c3c51941faf639068cfb0878dcdb4
kennychiangcom <kenny@chiang.com>
2022-09-01 01:20:22 +0100

    IsFromManchester refinement

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -6,7 +6,7 @@ namespace Exercises
 {
     public class Exercise002
     {
-        public bool IsFromManchester(Person person) => (person != null) && person.ToString().Any() && person.FromCity.Equals("Manchester");
+        public bool IsFromManchester(Person person) => person != null && person.ToString().Any() && person.FromCity.ToLower() == "manchester";
         public bool CanWatchFilm(Person person, int ageLimit) => person.Age >= ageLimit;
     }
 }
